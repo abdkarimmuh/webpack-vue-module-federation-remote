4e3350a906c0772718d13041aac85428f3f2828c
Muhammad Abdul Karim <alkariim.tridek@gmail.com>
2023-09-29 16:17:40 +0700

    bismillah

---
+++ src/pages/AboutView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>This is an about page lalala</h1>
+  </div>
+</template>
+<script></script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,55 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+const webpack = require("webpack");
+// const path = require("path");
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  pages: {
+    index: {
+      entry: "./src/index.js",
+    },
+  },
+  publicPath: "auto",
+  configureWebpack: {
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          defaultVendors: {
+            name: "chunk-vendors",
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10,
+            chunks: "async",
+            reuseExistingChunk: true,
+          },
+          common: {
+            name: "chunk-common",
+            minChunks: 2,
+            priority: -20,
+            chunks: "async",
+            reuseExistingChunk: true,
+          },
+        },
+      },
+    },
+    plugins: [
+      new webpack.container.ModuleFederationPlugin({
+        name: "remote",
+        filename: "remoteEntry.js",
+        exposes: {
+          "./HelloWorld.vue": "./src/components/HelloWorld.vue",
+          "./AboutView.vue": "./src/pages/AboutView.vue",
+        },
+        shared: {
+          vue: {
+            singleton: true,
+          },
+        },
+      }),
+    ],
+    // resolve: {
+    //   alias: {
+    //     "@": path.resolve(__dirname, "./src"),
+    //   },
+    // },
+  },
+  transpileDependencies: true,
+});
